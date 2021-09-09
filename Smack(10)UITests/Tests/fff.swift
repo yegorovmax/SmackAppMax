8389b0a4857ec13b9b1743fa49050b132ece4935
Max Egorov <yegorovmax@yahoo.com>
2021-09-09 00:08:36 -0700

    DO NOT COMMIT DO NOT MERGE

---
+++ Smack(10)UITests/Tests/fff.swift
@@ -0,0 +1,9 @@
+//
+//  fff.swift
+//  Smack(10)UITests
+//
+//  Created by Maksim Egorov on 12/10/19.
+//  Copyright © 2019 Maksim Egorov. All rights reserved.
+//
+
+import Foundation
---
+++ Smack(10)UITests/Utils/Stub/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  Smack(10)UITests
+//
+//  Created by Maksim Egorov on 12/10/19.
+//  Copyright © 2019 Max E. All rights reserved.
+//
+
+import Foundation
